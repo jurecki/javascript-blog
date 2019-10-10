049ad82cf0b4ec9abf56392c640b69d693698f0e
Pawel Jurecki <jurecki.pawel@gmail.com>
2019-10-10 08:13:29 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
+const titleClickHandler = function(event) {
+    const clickedElement = this;
 
-const titleClickHandler = function() {
-    //console.log('Link was clicked!', event);
+	/* remove class 'active' form all article links */
+	const activeLinks = document.querySelectorAll('.titles a.active');
 
-    /* remove class 'active' form all article links */
-    const activeLinks = document.querySelectorAll('.titles a.active');
-
-    for(let activeLink of activeLinks){
-      activeLink.classList.remove("active");
-    }
+	for (let activeLink of activeLinks) {
+		activeLink.classList.remove('active');
+	}
 
     /* add class 'active' to the clicked link */
+    
+    clickedElement.classList.add('active')
 
     /* remove class 'active' form all article */
-    
     const activeArticles = document.querySelectorAll('.post');
 
-    for(let activeArticle of activeArticles){
-        activeArticle.classList.remove("active");
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
     }
-/* get href attribut form the clicked link */
 
-/* find the correct article using the selector  (value of 'href' attribute) */
+	/* get href attribut form the clicked link */
 
-/* add class 'active' to the correct article */
-}
+	/* find the correct article using the selector  (value of 'href' attribute) */
+
+	/* add class 'active' to the correct article */
+};
 
 const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+	link.addEventListener('click', titleClickHandler);
 }
